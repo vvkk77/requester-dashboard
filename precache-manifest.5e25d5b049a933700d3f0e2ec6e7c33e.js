0a8dcf1c3fe58797a9b4cc7e567bee30d2aa31e3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-09 18:12:14 +0000

    Deploy vvkk77/requester-dashboard to github.com/vvkk77/requester-dashboard.git:gh-pages

---
+++ precache-manifest.5e25d5b049a933700d3f0e2ec6e7c33e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07258c88774d242d2fe8",
+    "url": "/requester-dashboard/css/app.bf053447.css"
+  },
+  {
+    "revision": "4049a8576ce5636f440e74b7f7bbe9fe",
+    "url": "/requester-dashboard/e-pass-template.csv"
+  },
+  {
+    "revision": "423d5a42de0d7207f7d70a052f86593c",
+    "url": "/requester-dashboard/img/login-bg.423d5a42.png"
+  },
+  {
+    "revision": "de305c552c19922c1a5aeaa49d0bb9e5",
+    "url": "/requester-dashboard/img/no-request.de305c55.png"
+  },
+  {
+    "revision": "a70cbdd67aebcc8b92d25dba4c652c74",
+    "url": "/requester-dashboard/index.html"
+  },
+  {
+    "revision": "07258c88774d242d2fe8",
+    "url": "/requester-dashboard/js/app.e188144d.js"
+  },
+  {
+    "revision": "eff319c18712be0838b7",
+    "url": "/requester-dashboard/js/chunk-vendors.c09f76fd.js"
+  },
+  {
+    "revision": "ae69ceac071030e56858757e5aff6d5e",
+    "url": "/requester-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/requester-dashboard/robots.txt"
+  }
+]);
